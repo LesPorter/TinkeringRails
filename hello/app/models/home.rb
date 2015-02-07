1427ad723824e1bc6779e3950550c1a76fd0700b
LesPorter <les@lesporter.com>
2015-02-07 22:59:35 +0000

    Added a happy birthday message when my birthday occurs

--- hello/app/models/home.rb
+++ hello/app/models/home.rb
@@ -4,6 +4,11 @@ class Home
     name = "Les"
   end
   
+  def now
+    now = Date.today
+    # now = Date.new(2015,3,1)  # Uncomment this line to test the birthday message
+  end
+  
   def birthdate
     birthdate = Date.new(1977,3,1)
   end
@@ -12,15 +17,12 @@ class Home
     birthdate_this_year = now.year.to_s + "-" + birthdate.month.to_s + "-" + birthdate.day.to_s 
   end
   
-  def now
-    now = Date.today
-  end
-  
   def age
-    age = now.year - birthdate.year
     # If birthday has not passed in current year then correct age by -1
     if now < Date.parse(birthdate_this_year) then
-      age = age - 1
+      age = now.year - birthdate.year - 1
+    else
+      age = now.year - birthdate.year
     end
   end
     
@@ -32,7 +34,7 @@ class Home
       year_for_next_birthdate = now.year + 1
     end
     next_birthdate = year_for_next_birthdate.to_s + "-" + birthdate.month.to_s + "-" + birthdate.day.to_s     
-    days_until_bday = (next_birthdate.to_date - now).to_i
+      days_until_bday = (next_birthdate.to_date - now).to_i
   end
   
 end

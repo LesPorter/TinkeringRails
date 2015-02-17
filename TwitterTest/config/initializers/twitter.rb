48a7ec0c27517be5769e06ae0c79f61fd12763b3
LesPorter <les@lesporter.com>
2015-02-17 04:55:07 +0000

    Created Twitter app to begin playing with API

---
+++ TwitterTest/config/initializers/twitter.rb
@@ -0,0 +1,6 @@
+$twitter = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "hidden"
+  config.consumer_secret     = "hidden"
+  config.access_token        = "hidden"
+  config.access_token_secret = "hidden"
+end

0b137c87cde4a9de2355d2f47480a5ec4e8d198a
LesPorter <les@lesporter.com>
2015-03-13 02:39:22 +0000

    I started to debug the latest error but didn't get far.

--- TwitterAPITest/app/controllers/pages_controller.rb
+++ TwitterAPITest/app/controllers/pages_controller.rb
@@ -4,7 +4,7 @@ class PagesController < ApplicationController
   
   def home
     # This one worked. Keep digging here.
-    data = open("https://api.twitter.com/1.1/statuses/show.json?id=XXX&oauth_consumer_key=XXX&oauth_nonce=XXXX&oauth_signature=XXXX&oauth_signature_method=XXX&oauth_timestamp=1426120765&oauth_token=XXX&oauth_version=1.0").read
+    # data = open("https://api.twitter.com/1.1/statuses/show.json?id=XXX&oauth_consumer_key=XXX&oauth_nonce=XXXX&oauth_signature=XXXX&oauth_signature_method=XXX&oauth_timestamp=1426120765&oauth_token=XXX&oauth_version=1.0").read
     @results = data["results"]
   end
 end

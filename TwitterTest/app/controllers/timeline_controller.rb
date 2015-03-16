15760bfb4e85d6f4f80a9e9d95c054a7e6562707
LesPorter <les@lesporter.com>
2015-03-16 22:02:18 +0000

    Cleaned up my controller a bit for sharing.

--- TwitterTest/app/controllers/timeline_controller.rb
+++ TwitterTest/app/controllers/timeline_controller.rb
@@ -1,9 +1,5 @@
 class TimelineController < ApplicationController
-  def lookup
-    # @profile = current_user.user("FAIL")
-    # @tweet = current_user.status(27558893223)
-    # @profile_timeline = current_user.user_timeline("FAIL")  # Doesn't work
-    
+  def lookup    
     
     # get user id from user name 
     @id = current_user.user('FAIL').id
@@ -11,12 +7,12 @@ class TimelineController < ApplicationController
     # Get profile description
     @desc = current_user.user('FAIL').description
     
-    #@tweets = current_user.user_timeline('FAIL', {count: 3})
+    # @tweets = current_user.user_timeline('FAIL', {count: 3})
     
     # user_timeline('FAIL', options = {}) ⇒ Array<Twitter::Tweet>
     
     # read user's timeline by id
-    #@timeline = current_user.user_timeline @id
+    # @timeline = current_user.user_timeline @id
 
     # read user's timeline of yours
     # your_timeline = current_user.user_timeline
